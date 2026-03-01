18e8ce5b9a5a3184d7e25441c57a4f18a5336717
Romain Ruetschi <github@romac.me>
2026-03-01 10:48:29 +0100

    Move macros in their own module

---
+++ src/macros.rs
@@ -0,0 +1,39 @@
+#[macro_export]
+macro_rules! Effects {
+    [$($effect:ty),*] => {
+        ::frunk_core::Coprod!($($effect),*)
+    };
+}
+
+macro_rules! run {
+    ($lt:lifetime, $effs:ty, $co:expr, $effect:pat => $handle:expr) => {{
+        let mut co = ::std::pin::pin!($co);
+
+        let mut yielded = co.as_mut().resume_with($crate::effect::Start);
+
+        loop {
+            match yielded {
+                ::fauxgen::GeneratorState::Complete(value) => break Ok(value),
+
+                ::fauxgen::GeneratorState::Yielded(effect) => {
+                    let $effect = match effect {
+                        ::frunk_core::coproduct::Coproduct::Inl(_) => unreachable!(),
+                        ::frunk_core::coproduct::Coproduct::Inr(subeffect) => subeffect,
+                    };
+
+                    let resume: $crate::control::CoControl<$lt, $effs> = $handle;
+                    match resume {
+                        $crate::control::CoControl::Cancel => {
+                            break Err($crate::control::Cancelled);
+                        }
+                        $crate::control::CoControl::Resume(r) => {
+                            yielded = co
+                                .as_mut()
+                                .resume(::frunk_core::coproduct::Coproduct::Inr(r))
+                        }
+                    }
+                }
+            }
+        }
+    }};
+}
